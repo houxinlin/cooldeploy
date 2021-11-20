b2bf0fa276189467668f6fbb4a2d697340310265
hxl <2606710413@qq.com>
2021-11-20 14:26:12 +0800

    修改登录Bug

--- src/main/kotlin/com/hxl/cooldeploy/resolver/JsonObjectValueHandlerMethodArgumentResolver.kt
+++ src/main/kotlin/com/hxl/cooldeploy/resolver/JsonObjectValueHandlerMethodArgumentResolver.kt
@@ -2,6 +2,7 @@ package com.hxl.cooldeploy.resolver
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.type.TypeFactory
+import com.hxl.cooldeploy.service.impl.ProjectServiceImpl
 import org.springframework.core.MethodParameter
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
@@ -11,6 +12,7 @@ import org.springframework.web.util.ContentCachingRequestWrapper
 import javax.servlet.http.HttpServletRequest
 
 class JsonObjectValueHandlerMethodArgumentResolver : HandlerMethodArgumentResolver {
+    var log = org.slf4j.LoggerFactory.getLogger(JsonObjectValueHandlerMethodArgumentResolver::class.java)
     var threadLocal = ThreadLocal<String>();
     override fun supportsParameter(parameter: MethodParameter): Boolean {
         return parameter.hasParameterAnnotation(JsonObjectValue::class.java)
@@ -30,6 +32,7 @@ class JsonObjectValueHandlerMethodArgumentResolver : HandlerMethodArgumentResolv
 
         var string = requestWrapper.inputStream.bufferedReader()
         val o: HashMap<String?, Any?>? = mapper.readValue(string, mapType)
+        log.info("resolveArgument->" + o.toString())
         return o?.get(key);
     }
 }
