376149b753c8ace40b4ff3caf275ebaa64679891
hxl <2606710413@qq.com>
2021-11-20 13:04:37 +0800

    修改登陆bug

--- src/main/kotlin/com/hxl/cooldeploy/config/SystemDefaultConfig.kt
+++ src/main/kotlin/com/hxl/cooldeploy/config/SystemDefaultConfig.kt
@@ -1,5 +1,6 @@
 package com.hxl.cooldeploy.config
 
+import com.hxl.cooldeploy.controller.SystemController
 import com.hxl.cooldeploy.kotlin.extent.toFile
 import com.hxl.cooldeploy.kotlin.extent.toJsonString
 import com.hxl.cooldeploy.kotlin.extent.toMap
@@ -9,22 +10,39 @@ import java.util.*
 
 class SystemDefaultConfig : Properties() {
     init {
-        putIfAbsent("login_passwd", "cooldeploy")
+        //初始化必要目录
+        DirectoryUtils.initNecessaryFile()
+        var localMap = DirectoryUtils.getSystemConfigFile().toFile().toMap()
+        if (localMap.isEmpty()) {
+            default()
+            storage()
+        }
+    }
+
+    fun default(): SystemDefaultConfig {
+        put("login_passwd", "cooldeploy")
+        return this;
     }
 
     fun init() {
-        DirectoryUtils.initNecessaryFile()
+
+        //从本地文件加载配置
         this.putAll(DirectoryUtils.getSystemConfigFile().toFile().toMap())
-        FileUtils.writeString(DirectoryUtils.getSystemConfigFile(), this.toJsonString())
+
     }
 
     override fun put(key: Any?, value: Any?): Any? {
+        //把属性都存如系统值
         System.setProperty(key as String, value as String)
         return super.put(key, value)
     }
 
+    /**
+     * 存储
+     */
     fun storage() {
         var mutableMapOf = mutableMapOf<String, String>()
+        //从系统的属性中拿值。
         this.keys.forEach { mutableMapOf[it.toString()] = System.getProperty(it.toString()) }
         FileUtils.writeString(DirectoryUtils.getSystemConfigFile(), mutableMapOf.toJsonString())
     }
